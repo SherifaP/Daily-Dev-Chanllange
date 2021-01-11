4026f9de1b9261eeec41eb8509cfa97ba1af2fe9
misspeghama <sherifangong@gmail.com>
2021-01-12 00:31:30 +0100

    my commit

---
+++ Day3 Separate Character of a string/character.js
@@ -0,0 +1,5 @@
+let name = prompt("enter your name")
+let split = [];
+for (i = 0; i < Name.length; i++)
+    split[i] = Name[i];
+console.log(split); 
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-var area = 1000;
-var pii = 3.14;
-radius = area/pii*2
-console.log(radius);
-
+// var area = 1000;
+// var pii = 3.14;
+// radius = area/pii*2
+// console.log(radius);
 
