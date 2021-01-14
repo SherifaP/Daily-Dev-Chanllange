5237ed91cfe8aae8ccb3a8cb4381e391114be3f0
misspeghama <sherifangong@gmail.com>
2021-01-14 23:36:42 +0100

    my commit

---
+++ Day5 Vowels/vowel.js
@@ -0,0 +1,9 @@
+const myArr = "Sherifa";
+const emptyArr = [];
+for(let i = 0; i < myArr.length; i++){
+   emptyArr[i] = myArr[i];
+    if(emptyArr[i]=== 'a' ||emptyArr[i]=== 'e' ||emptyArr[i]=== 'i' ||emptyArr[i]=== 'o' ||emptyArr[i]=== 'u'){
+
+        console.log(emptyArr[i]);
+    }
+}
